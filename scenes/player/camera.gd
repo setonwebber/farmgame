e203da0dbff0e66601db00c0f63bb19d49f3bcee
Flynn <40328751+floono@users.noreply.github.com>
2024-12-16 16:03:43 +0800

    implemented independent character and camera movement, fixed clamping issue

---
+++ scenes/player/camera.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@onready var character: CharacterBody3D = $"../Character"
+
+var mouse_sensitivity: float = 0.1
+var yaw: float = rotation_degrees.y
+var pitch: float = rotation_degrees.x
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+func _input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		yaw = yaw - event.relative.x * mouse_sensitivity
+		pitch = clamp(pitch - event.relative.y * mouse_sensitivity, -89, 89)
+		
+		rotation_degrees = Vector3(pitch, yaw, 0)
+
+func _process(delta: float):
+	print("yaw: ", yaw, "pitch: ", pitch)
+	global_position = character.global_position
+	if Input.is_action_just_pressed("disable_mouse"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- scenes/player/character.gd
+++ scenes/player/character.gd
@@ -4,13 +4,14 @@ extends CharacterBody3D
 @export var JUMP_VELOCITY: float = 4.5
 
 func _physics_process(delta: float) -> void:
-	var camera_pivot: Node3D = $CameraPivot
+	var camera_pivot: Node3D = $"../CameraPivot"
 	var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
 	var direction: Vector3 = (camera_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var prev_direction: Vector3
 	
 	# Gravity calculations
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity = velocity + get_gravity() * delta
 
 	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -18,17 +19,17 @@ func _physics_process(delta: float) -> void:
 	
 	# Sprinting
 	if Input.is_action_just_pressed("sprint") and is_on_floor():
-		SPEED *= 2
+		SPEED = SPEED * 2
 	elif Input.is_action_just_released("sprint"):
-		SPEED /= 2
+		SPEED = SPEED / 2
 
 	# Movement
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		rotation.y = atan2(velocity.x, velocity.z)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED )
-		velocity.z = move_toward(velocity.z, 0, SPEED )
-
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
