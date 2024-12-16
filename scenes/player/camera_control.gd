eb5febcd59c80b713787be3ec214bbeb099f7e14
Seton <110365299+setonwebber@users.noreply.github.com>
2024-12-16 13:08:53 +0800

    added freecam and sprinting

--- scenes/player/camera_control.gd
+++ scenes/player/camera_control.gd
@@ -5,15 +5,17 @@ var yaw: float = 0
 var pitch: float = 0
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 func _input(event: InputEvent):
-	if event is InputEventMouseMotion:
+	# if right click is held, the camera move will start.
+	if Input.is_action_pressed("freeaim") and event is InputEventMouseMotion:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		yaw = yaw - event.relative.x * mouse_sensitivity
 		pitch = clamp(pitch - event.relative.y * mouse_sensitivity, -89, 89)
 		
 		rotation_degrees = Vector3(pitch, yaw, 0)
 
 func _process(delta: float):
-	if Input.is_action_just_pressed("ui_cancel"): 
+	if Input.is_action_just_released("freeaim"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -15,12 +15,20 @@ func _physics_process(delta: float) -> void:
 	# Jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	# Sprinting
+	if Input.is_action_just_pressed("sprint") and is_on_floor():
+		SPEED *= 2
+	elif Input.is_action_just_released("sprint"):
+		SPEED /= 2
 
+	# Movement
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED )
+		velocity.z = move_toward(velocity.z, 0, SPEED )
+
 
 	move_and_slide()
