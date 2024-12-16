8b31ac7ca427e5b7ac82658c279155933cbd9556
Seton <110365299+setonwebber@users.noreply.github.com>
2024-12-16 16:13:45 +0800

    fixed merge

--- scenes/player/camera.gd
+++ scenes/player/camera.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@onready var character: CharacterBody3D = $"../Character"
+@onready var character: CharacterBody3D = $"../CharacterBody3D"
 
 var mouse_sensitivity: float = 0.1
 var yaw: float = rotation_degrees.y
@@ -17,7 +17,7 @@ func _input(event: InputEvent):
 		rotation_degrees = Vector3(pitch, yaw, 0)
 
 func _process(delta: float):
-	print("yaw: ", yaw, "pitch: ", pitch)
+	#print("yaw: ", yaw, "pitch: ", pitch)
 	global_position = character.global_position
 	if Input.is_action_just_pressed("disable_mouse"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
