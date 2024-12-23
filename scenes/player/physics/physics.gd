c3cb377c147474e189d9b9a78b9875608e85856e
Flynn <40328751+floono@users.noreply.github.com>
2024-12-23 20:03:42 +0800

    updated player movement

--- scenes/player/physics/physics.gd
+++ scenes/player/physics/physics.gd
@@ -36,7 +36,28 @@ func _physics_process(delta: float) -> void:
 		physics_body.velocity.z = direction.z * speed
 		physics_body.rotation.y = atan2(direction.x, direction.z)
 	else:
+<<<<<<< Updated upstream
 		physics_body.velocity.x = move_toward(physics_body.velocity.x, 0, speed)
 		physics_body.velocity.z = move_toward(physics_body.velocity.z, 0, speed)
+=======
+		# Jumping
+		if Input.is_action_just_pressed("jump") and physics_body.is_on_floor():
+			physics_body.velocity.y = jump_velocity
+		
+		# Sprinting
+		if Input.is_action_just_pressed("sprint"):
+			speed = speed * 2
+		elif Input.is_action_just_released("sprint"):
+			speed = speed / 2
+
+		# Movement
+		if direction != Vector3.ZERO:
+			physics_body.velocity.x = direction.x * speed
+			physics_body.velocity.z = direction.z * speed
+			physics_body.rotation.y = lerp_angle(physics_body.rotation.y, atan2(direction.x, direction.z), rotation_speed * delta)
+		else:
+			physics_body.velocity.x = move_toward(physics_body.velocity.x, 0, speed)
+			physics_body.velocity.z = move_toward(physics_body.velocity.z, 0, speed)
+>>>>>>> Stashed changes
 	
 	physics_body.move_and_slide()
