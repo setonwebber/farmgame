d55f0de6711b28df9e85da19dea2c1c1ec65a5db
Flynn <40328751+floono@users.noreply.github.com>
2024-12-23 20:09:32 +0800

    changed signal paths for plot collision signals

--- scenes/farm/farm_plot/plot_area.gd
+++ scenes/farm/farm_plot/plot_area.gd
@@ -2,8 +2,8 @@ extends Area3D
 
 func _on_body_entered(body):
 	if body is PlayerPhysicsBody:
-		body.get_parent().get_parent().farming.farm_plot = $".."
+		body.get_node("../../Farming").farm_plot = $".."
 
 func _on_body_exited(body):
 	if body is PlayerPhysicsBody:
-		body.get_parent().get_parent().farming.farm_plot = null
+		body.get_node("../../Farming").farm_plot = null
